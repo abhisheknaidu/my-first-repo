5e6805a975d107d1472b883ef1de941df04d3d75
abhishek <abhishek.naidu@geappliances.com>
2019-10-01 16:32:43 +0530

    Commit

--- HelloWorld/HelloWorld/AppDelegate.swift
+++ HelloWorld/HelloWorld/AppDelegate.swift
@@ -7,6 +7,10 @@
 //
 
 import UIKit
+import AppCenter
+import AppCenterAnalytics
+import AppCenterCrashes
+
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +20,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        MSAppCenter.start("f8604719-e743-4648-8a0a-3f153bb6f538", withServices:[
+            MSAnalytics.self,
+            MSCrashes.self
+            ])
         return true
     }
 
